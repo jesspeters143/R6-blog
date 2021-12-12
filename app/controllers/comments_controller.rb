4eca25b084a77b62535255e9ab075e29d7c76320
Jessica Peters <jesspeters143@gmail.com>
2021-12-12 10:29:51 -0800

    second commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,27 @@
 class CommentsController < ApplicationController
-    def create 
-        @post = Post.find(parmas[:post_id])
-        @comment = @post.comments.create(params[:comment].permit(:name, :body))
-        redirect_to post_path(@post)
+  # No `new` action because form is provided by PostsController#show
+  def create
+    @post = Post.find(params[:post_id])
+    # Create associated model, just like we did in the console before
+    @comment = @post.comments.create(comment_params)
+    # We want to show the comment in the context of the Post
+    redirect_to @post
+  end
+    def edit
+      @comment = Comment.find(params[:id])
     end
-end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        redirect_to @post
+    end
+
+    private
+    def comment_params
+      params.require(:comment).permit(:name, :content)
+    end
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+  end
